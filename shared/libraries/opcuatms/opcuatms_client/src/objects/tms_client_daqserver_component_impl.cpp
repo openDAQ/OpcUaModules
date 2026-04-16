6e354814c1e4a8c1038e7681e1325d4fadd8fe8b
NikolaiShipilov <127689162+NikolaiShipilov@users.noreply.github.com>
2026-04-16 11:37:28 +0200

    Add support for enable disable discovery (#21)
    * Add enable/disable discovery for server components
    * Update opendaq commit hash

--- shared/libraries/opcuatms/opcuatms_client/src/objects/tms_client_daqserver_component_impl.cpp
+++ shared/libraries/opcuatms/opcuatms_client/src/objects/tms_client_daqserver_component_impl.cpp
@@ -1,6 +1,7 @@
 #include <coretypes/validation.h>
 #include <opendaq/custom_log.h>
 #include <opcuatms_client/objects/tms_client_daqserver_component_impl.h>
+#include <opcuatms/exceptions.h>
 
 BEGIN_NAMESPACE_OPENDAQ_OPCUA_TMS
 using namespace opcua;
@@ -17,7 +18,42 @@ TmsClientDaqServerComponentImpl::TmsClientDaqServerComponentImpl(const ContextPt
 
 ErrCode TmsClientDaqServerComponentImpl::enableDiscovery()
 {
-    return DAQ_MAKE_ERROR_INFO(OPENDAQ_ERR_NOTIMPLEMENTED);
+    const ErrCode errCode = daqTry([&]()
+    {
+        const auto methodNodeId = getNodeId("EnableDiscovery");
+
+        auto request = OpcUaCallMethodRequest();
+        request->objectId = nodeId.copyAndGetDetachedValue();
+        request->methodId = methodNodeId.copyAndGetDetachedValue();
+        request->inputArgumentsSize = 0;
+
+        auto response = client->callMethod(request);
+
+        if (response->statusCode != UA_STATUSCODE_GOOD)
+            throw OpcUaGeneralException();
+    });
+    OPENDAQ_RETURN_IF_FAILED(errCode);
+    return errCode;
+}
+
+ErrCode TmsClientDaqServerComponentImpl::disableDiscovery()
+{
+    const ErrCode errCode = daqTry([&]()
+    {
+        const auto methodNodeId = getNodeId("DisableDiscovery");
+
+        auto request = OpcUaCallMethodRequest();
+        request->objectId = nodeId.copyAndGetDetachedValue();
+        request->methodId = methodNodeId.copyAndGetDetachedValue();
+        request->inputArgumentsSize = 0;
+
+        auto response = client->callMethod(request);
+
+        if (response->statusCode != UA_STATUSCODE_GOOD)
+            throw OpcUaGeneralException();
+    });
+    OPENDAQ_RETURN_IF_FAILED(errCode);
+    return errCode;
 }
 
 ErrCode TmsClientDaqServerComponentImpl::stop()
