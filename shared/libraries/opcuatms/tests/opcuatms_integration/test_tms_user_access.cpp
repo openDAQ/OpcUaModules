7c5a408b66ce0413cd61eead84fced841d05d844
Denis Erokhin <denis.erokhin@opendaq.com>
2026-07-29 14:05:40 +0200

    undo

--- shared/libraries/opcuatms/tests/opcuatms_integration/test_tms_user_access.cpp
+++ shared/libraries/opcuatms/tests/opcuatms_integration/test_tms_user_access.cpp
@@ -315,12 +315,7 @@ TEST_P(TmsUserAccessPTest, CommonChecks)
     DevicePtr mockDevice = clientDevice.getDevices()[1];
     FunctionBlockPtr mockFb = clientDevice.getFunctionBlocks()[0];
 
-    // one private signal in MockFunctionBlockImpl. and one in MockPhysicalDeviceImpl
-    // Server Device may also expose nested DaqDeviceInfo, which is not mirrored over OPC UA TMS.
-    SizeT serverPropCount = device.getAllProperties().getCount();
-    if (device.hasProperty("DaqDeviceInfo"))
-        --serverPropCount;
-    EXPECT_EQ(serverPropCount - 1, mockDevice.getAllProperties().getCount());
+    EXPECT_EQ(device.getAllProperties().getCount() - 1, mockDevice.getAllProperties().getCount());
     EXPECT_EQ(fb.getAllProperties().getCount(), mockFb.getAllProperties().getCount());
 
     EXPECT_EQ(device.getSignals().getCount() - 1, mockDevice.getSignals().getCount());
